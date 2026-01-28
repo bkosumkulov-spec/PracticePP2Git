c2435e0082cc63ce9e106b2d7f7bf9d483c80348
Koshumkulov Beksultan <bkosumkulov@gmail.com>
2026-01-28 15:33:50 +0500

    Ashwaganda

---
+++ practice 1/task1.py
@@ -0,0 +1,13 @@
+#101 The Greeting Echo
+# a = str(input())
+# print("Hello, " + a + "!")
+
+#102 Custom Separators
+# a = str(input())
+# b = str(input())
+# print(a + "***" + b)
+
+# 103 Int or String
+"""
+Docstring for practice 1.task1
+"""
