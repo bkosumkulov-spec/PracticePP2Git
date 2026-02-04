39c8302067e30c3fcef0faf7075106867b473574
Koshumkulov Beksultan <bkosumkulov@gmail.com>
2026-02-04 13:58:37 +0500

    Ashwaganda

---
+++ practice2/702/1.py
@@ -0,0 +1,22 @@
+#201 leap year
+# year = int(input())
+# if year % 4 == 0 and year % 100 > 1 or year % 400 == 0:
+#     print("YES")
+# else:
+#     print("NO")    
+
+#202 Sum
+# n = int(input())
+# sum = 0
+# for i in range(1,n+1):
+#     sum += i
+# print(sum)    
+
+#203 Sum of numbers
+n = int(input())
+i = 0
+sum = 0
+while i <= n:
+    sum += i
+    i += 1
+print(sum)
